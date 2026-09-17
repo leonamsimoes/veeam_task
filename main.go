99ca91e659b5e2c54263dda67f08c6c743f895eb
simoes <leonam.simoes@gmail.com>
2026-09-17 22:11:34 +0100

    now working

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"errors"
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -12,59 +13,71 @@ import (
 )
 
 func main() {
-	lg := log.Default()
+	sourcePath := flag.String("source", "", "path to the source file")
+	replicaPath := flag.String("replica", "", "path to the replica file")
+	logPath := flag.String("log", "", "path to the log file")
+	interval := flag.Int("interval", 1, "interval to run (periodically), in hours, default 1 hour")
+	flag.Parse()
 
-	err := creatingFile("/home/simoes/projects/github.com/veeam_task/logs.txt", lg)
+	if *sourcePath == "" || *replicaPath == "" || *logPath == "" {
+		fmt.Fprintln(os.Stderr, "-source, -replica and -log are all required")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	lg, logFile := logging(*logPath)
+	defer logFile.Close()
+
+	periodically := time.Hour * time.Duration(*interval)
+
+	ticker := time.NewTicker(periodically)
+	defer ticker.Stop()
+
+	for {
+		runningPeriodically(*sourcePath, *replicaPath, lg)
+		<-ticker.C
+	}
+}
+
+func runningPeriodically(sourcePath, replicaPath string, lg *log.Logger) {
+	data, err := checkingFile(sourcePath, replicaPath, lg)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	data, err := checkingFile(lg)
+	err = replicatingFile(replicaPath, data, lg)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	err = replicatingFile(data, lg)
-	if err != nil {
-		return
-	}
 }
 
-func runningPeriodically(day int, lg *log.Logger) bool {
-	if time.Now().Day() == day {
-		return true
-	}
+func replicatingFile(replicaPath string, dataSource []byte, lg *log.Logger) error {
+	lg.Printf("replicatingFile: writing %d bytes to %s", len(dataSource), replicaPath)
 
-	return false
-}
-
-func replicatingFile(dataSource []byte, lg *log.Logger) error {
-	err := os.WriteFile("/home/simoes/projects/github.com/veeam_task/replica.txt", dataSource, os.ModeAppend)
+	err := os.WriteFile(replicaPath, dataSource, os.ModeAppend)
 	if err != nil {
+		lg.Printf("replicatingFile: failed to write %s: %v", replicaPath, err)
 		return err
 	}
 
 	return nil
 }
 
-func checkingFile(lg *log.Logger) ([]byte, error) {
-	filenameSource := "/home/simoes/projects/github.com/veeam_task/source.txt"
-	dataSource, err := readingFile(filenameSource, lg)
-	if err != nil {
-		return nil, err
-	}
-	fmt.Printf("\nDATA Source:%s\n", dataSource)
+func checkingFile(sourcePath, replicaPath string, lg *log.Logger) ([]byte, error) {
+	lg.Println("checkingFile: comparing source and replica")
 
-	filenameReplica := "/home/simoes/projects/github.com/veeam_task/replica.txt"
-	dataReplica, err := readingFile(filenameReplica, lg)
+	dataSource, err := readingFile(sourcePath, lg)
 	if err != nil {
 		return nil, err
 	}
 
-	fmt.Printf("\nDATA Replica:%s\n", dataReplica)
+	dataReplica, err := readingFile(replicaPath, lg)
+	if err != nil {
+		return nil, err
+	}
 
 	if bytes.Equal(dataSource, dataReplica) {
-		fmt.Printf("\nThe files are equal.\nData:%s", dataSource)
+		lg.Printf("the files are equal, data:%s", dataSource)
 	}
 
 	return dataSource, nil
@@ -72,13 +85,16 @@ func checkingFile(lg *log.Logger) ([]byte, error) {
 
 func readingFile(filename string, lg *log.Logger) ([]byte, error) {
 	fp := filepath.Clean(filename)
+	lg.Printf("readingFile: reading %s", fp)
 
 	data, err := os.ReadFile(fp)
 	if err != nil {
 		if !strings.Contains(err.Error(), "no such file or directory") {
+			lg.Printf("readingFile: failed to read %s: %v", fp, err)
 			return nil, errors.Join(fmt.Errorf("could not read the file"), err)
 		}
 
+		lg.Printf("readingFile: %s does not exist, creating it", fp)
 		err := creatingFile(fp, lg)
 		if err != nil {
 			return nil, err
@@ -89,10 +105,26 @@ func readingFile(filename string, lg *log.Logger) ([]byte, error) {
 }
 
 func creatingFile(filename string, lg *log.Logger) error {
+	lg.Printf("creatingFile: creating %s", filename)
+
 	_, err := os.Create(filename)
 	if err != nil {
+		lg.Printf("creatingFile: failed to create %s: %v", filename, err)
 		return errors.Join(fmt.Errorf("could not create the file"), err)
 	}
 
 	return nil
+}
+
+func logging(logPath string) (*log.Logger, *os.File) {
+	logFile, err := os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	lg := log.New(logFile, "", log.LstdFlags)
+
+	lg.Printf("\nlog ID: [%d]\n", time.Now().Unix())
+
+	return lg, logFile
 }
